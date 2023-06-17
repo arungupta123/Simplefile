756bd49e7ecadd12b8650f4f6ab51d728592a17f
ARUN <arun82870@gmail.com>
2023-06-17 19:55:16 +0530

    my first commit

--- src/main/java/Com/arun/git/Simple.java
+++ src/main/java/Com/arun/git/Simple.java
@@ -5,6 +5,9 @@ public class Simple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("arun");
+		
+		System.out.println("arun1");
+		System.out.println("arun2");
 
 	}
 
