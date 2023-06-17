6b2c80275693cad4d80b13f7c72afbcd35d9db45
ARUN <arun82870@gmail.com>
2023-06-17 21:32:27 +0530

    my third commit

--- src/main/java/Com/arun/git/Simple.java
+++ src/main/java/Com/arun/git/Simple.java
@@ -7,9 +7,8 @@ public class Simple {
 		System.out.println("arun");
 		
 		System.out.println("arun1");
-		System.out.println("arun2");
+		System.out.println("arun21");
 		
-		System.out.println("arun31");
 	}
 
 }
